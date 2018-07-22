8e976b74cdfbedc1e749980b7c68e77108eedc27
Alexander Della Polla Villarroel <alexander.dpv@gmail.com>
2018-07-22 13:49:54 -0500

    Implement search caching.

--- src/App.js
+++ src/App.js
@@ -23,26 +23,18 @@ const smallColumn = {
   width: '10%',
 }
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  }
-];
-
 class App extends Component {
   constructor(props) {
     super(props); // Sets this.props to be used in constructor
 
     this.state = {
-      result: null,
+      results: null,
+      searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null
     };
 
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
     this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
@@ -50,37 +42,33 @@ class App extends Component {
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
 
-  setSearchTopStories(result) {
-    const { hits, page } = result;
-
-    const oldHits = page !== 0 ? this.state.result.hits : [];
-    const updatedHits = [...oldHits, ...hits];
-
-    this.setState({
-      result: { hits: updatedHits, page }
-    });
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({error}));
   }
 
   componentDidMount() {
     const { searchTerm } = this.state;
-
+    this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
   }
 
   // Removes object from list
   onDismiss(id) {
+    const { searchKey, results} = this.state;
+    const { hits, page } = results[searchKey];
+
     const isNotId = (item) => item.objectID !== id;
-    const updatedHits = this.state.result.hits.filter(isNotId);
+    const updatedHits = hits.filter(isNotId);
 
     this.setState({
-      result: { ...this.state.result, hits: updatedHits }
+      results: { ...results, [searchKey]: { hits: updatedHits, page }}
     });
   }
 
@@ -90,13 +78,31 @@ class App extends Component {
 
   onSearchSubmit(event) {
     const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm);
+    this.setState({ searchKey: searchTerm });
+
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+
     event.preventDefault();
   }
 
+  setSearchTopStories(result) {
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+
+    const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
+    const updatedHits = [...oldHits, ...hits];
+
+    this.setState({
+      results: { ...results, [searchKey]: { hits: updatedHits, page }}
+    });
+  }
+
   render() {
-    const { searchTerm, result } = this.state;
-    const page = (result && result.page) || 0;
+    const { searchTerm, results, searchKey, error } = this.state;
+    const page = (results && results[searchKey] && results[searchKey].page) || 0;
+    const list = (results && results[searchKey] && results[searchKey].hits) || [];
 
     return (
       <div className="page">
@@ -108,14 +114,14 @@ class App extends Component {
           >
             Search:
           </Search>
-          { result ?
+          { results ?
             <div>
               <Table
-                list={result.hits}
+                list={list}
                 onDismiss={this.onDismiss}
               />
               <div className="interactions">
-                <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+                <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
                   More
                 </Button>
               </div>
@@ -123,6 +129,8 @@ class App extends Component {
           : null
           }
         </div>
+        { error ? <div className="interactions"><p>Something went wrong.</p></div>
+          : <Table list={list} onDismiss={this.onDismiss}/>}
       </div>
     );
   }
